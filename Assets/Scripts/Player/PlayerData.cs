49b090d9144dab4b4011892f7cc98d215872d78b
PowGrow <slideb910@gmail.com>
2023-01-23 09:32:31 +0300

    PlayerData,SceneContext,SceneInstaller

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+    public Transform Transform { get; private set; }
+    public bool IsAlive { get; private set; } = true;
+    public float Health { get; private set; }
+    private void Awake()
+    {
+        Transform = transform;
+        Health = _maxHealth;
+    }
+
+    public void GetDamage(float value)
+    {
+        Health -= value;
+        if(Health <= 0)
+        {
+            Health = 0;
+            IsAlive = false;
+            Debug.Log("Player are dead!");
+        }
+    }
+}
