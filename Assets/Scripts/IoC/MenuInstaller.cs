c1e3bacf4c19956178a76c80a634566c4bf6db50
PowGrow <slideb910@gmail.com>
2023-02-06 23:05:52 +0300

    Start game ui injected

--- Assets/Scripts/IoC/MenuInstaller.cs
+++ Assets/Scripts/IoC/MenuInstaller.cs
@@ -7,10 +7,14 @@ public class MenuInstaller : MonoInstaller
     private GameObject playerPrefab;
     [SerializeField]
     private Transform startPosition;
+    [SerializeField]
+    private GameObject startGameUi;
 
     public override void InstallBindings()
     {
         var player = Container.InstantiatePrefabForComponent<PlayerData>(playerPrefab, startPosition.position, Quaternion.identity, null);
         Container.Bind<PlayerData>().FromInstance(player).AsSingle();
+
+        Container.InstantiatePrefabForComponent<MainMenuUi>(startGameUi);
     }
 }
--- Assets/Scripts/UI/MainMenuUi.cs
+++ Assets/Scripts/UI/MainMenuUi.cs
@@ -1,12 +1,24 @@
+using HurricaneVR.Framework.Core.Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Zenject;
 
 public class MainMenuUi : MonoBehaviour
 {
     [SerializeField]
     private string sceneName;
+
+    private PlayerData _player;
+    private Canvas _canvas;
+
     [ContextMenu("StartGame")]
 
+    [Inject]
+    public void Construct(PlayerData player)
+    {
+        _player = player;
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadSceneAsync(sceneName);
@@ -15,5 +27,15 @@ public class MainMenuUi : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void Awake()
+    {
+        _canvas = GetComponent<Canvas>();
+    }
+
+    private void Start()
+    {
+        _canvas.worldCamera = _player.GetComponent<HVRPlayerController>().Camera.GetComponent<Camera>();
     }
 }
